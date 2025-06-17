f861efb1e719a618aa135d5add6a08dcac2247b4
Manish <manish2003sharma@gmail.com>
2025-06-17 14:40:55 +0530

    Weather_App

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import WeatherCloud from '../assets/weather-cloudy.png'
 
 const WeatherCard = ({ weatherDetails }) => {
-    console.log(weatherDetails);
+    // console.log(weatherDetails);
     return (
         <div className="waether_section">
             <div className="weather_card">
@@ -27,5 +28,19 @@ const WeatherCard = ({ weatherDetails }) => {
         </div>
     )
 }
+WeatherCard.propTypes = {
+    weatherDetails: PropTypes.shape({
+        current: PropTypes.shape({
+            temp_c: PropTypes.number.isRequired,
+            humidity: PropTypes.number.isRequired,
+            cloud: PropTypes.number.isRequired,
+        }).isRequired,
+        location: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            region: PropTypes.string.isRequired,
+            country: PropTypes.string.isRequired,
+        }).isRequired,
+    }).isRequired,
+};
 
-export default WeatherCard
+export default WeatherCard;
